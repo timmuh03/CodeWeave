5369448abb79c26c152eabff545dee9825ff643e
Timothy Muhlsetein <122745865+timmuh03@users.noreply.github.com>
2026-05-21 12:28:47 +0000

    Basic details load on consult term click

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,24 +7,72 @@ import {
 import "./App.css";
 
 function App() {
-  const [concepts, setConcepts] = useState([]);
-  
+  const [conceptsLight, setConcepts] = useState([]);
+  const [openedConcepts, setOpenedConcepts] = useState([]);
+  const [conceptsFull, setConceptsFull] = useState({});
 
   useEffect(() => {
-    fetchConcepts().then((data) => setConcepts(data));
-  }, [])
+    loadConceptList();
+  }, []);
+
+  async function loadConceptList() {
+    const list = await fetchConcepts();
+    setConcepts(list);
+  }
+
+  async function handleConceptTermClick(conceptId) {
+    const isOpen = openedConcepts.includes(conceptId);
+
+    if(isOpen) {
+      setOpenedConcepts((currentIds) => 
+        currentIds.filter((id) => id !== conceptId));
+      return;
+    }
+
+    setOpenedConcepts((currentIds) => [...currentIds, conceptId]);
+
+    if (conceptsFull.conceptId) {
+      return;
+    }
+
+    await loadConceptDetails(conceptId);
+  }
+
+  async function loadConceptDetails(conceptId) {
+    const details = await fetchConcept(conceptId);
+
+    setConceptsFull((currentConcepts) => ({
+      ...currentConcepts,
+      [conceptId]: details,
+    }));
+  }
   
   return (
     <main className="app">
       <header className="app-header">
         <h1>Concepts</h1>
       </header>
-      <section className="concept-list">
-        {concepts.map((concept) => (
-          <div key={concept.id} className="concept-term">
-            {concept.term}
-          </div>
-        ))}
+      <section className="concept-list-section">
+        <ul className="concept-list">
+          {conceptsLight.map((concept) => {
+            const isOpen = openedConcepts.includes(concept.id);
+            const fullConcept = conceptsFull[concept.id];
+      
+            return (
+            <li key={concept.id} className="concept-term"
+              onClick={() => handleConceptTermClick(concept.id)}
+            >
+              {concept.term}
+              
+              {isOpen && (
+                <div className="concept-description">
+                  {fullConcept ? fullConcept.description : "Loading..."}
+                </div>
+              )}
+            </li>
+            );
+          })}
+        </ul>
       </section>
     </main>
   )
